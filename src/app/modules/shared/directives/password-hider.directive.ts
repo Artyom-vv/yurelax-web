c048d9476de3eccb5ef872fb94f0518d77733990
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2023-03-11 16:40:07 +0300

    fix: directive simplify

--- src/app/modules/shared/directives/password-hider.directive.ts
+++ src/app/modules/shared/directives/password-hider.directive.ts
@@ -4,13 +4,11 @@ import {Directive, HostListener, Input} from "@angular/core";
   selector: '[passwordHider]',
 })
 export class PasswordHiderDirective {
-  @Input() ref!: HTMLInputElement;
   @Input() deps?: HTMLInputElement[];
   @HostListener('click') onClick() {
-    this.ref.type = this.ref.type === 'password' ? 'text' : 'password'
     if (Array.isArray(this.deps)) {
       this.deps.forEach(dep => {
-        dep.type = this.ref.type;
+        dep.type = dep.type === 'password' ? 'text' : 'password';
       })
     }
   }
