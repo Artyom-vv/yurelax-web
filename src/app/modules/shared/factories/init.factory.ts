0f6608694082c86d0d10687aec83f911a2bd27e2
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2026-08-24 13:48:31 +0300

    fix(web): restore Angular 18 production build

--- src/app/modules/shared/factories/init.factory.ts
+++ src/app/modules/shared/factories/init.factory.ts
@@ -1,4 +1,4 @@
-import { IconsService } from "src/app/services/icons.service";
+import { IconsService } from "../../../services/icons.service";
 
 export function appInitializer(service: IconsService): () => Promise<any> {
   return () => service.initIcons();
